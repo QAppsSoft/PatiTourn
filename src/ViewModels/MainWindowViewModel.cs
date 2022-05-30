88717071d04e10537ab6f9079949dfcf11cd9830
alex6dj <alexeiagueroalba@gmail.com>
2022-05-30 18:51:09 -0400

    Cleanup

--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -11,27 +11,30 @@ namespace ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public CompetitionsViewModel CompetitionsViewModel { get; }
-
         public MainWindowViewModel(CompetitionsViewModel competitionsViewModel)
         {
-            CompetitionsViewModel = competitionsViewModel ?? throw new ArgumentNullException(nameof(competitionsViewModel));
+            CompetitionsViewModel =
+                competitionsViewModel ?? throw new ArgumentNullException(nameof(competitionsViewModel));
 
             SearchCompetitions = ReactiveCommand.CreateFromTask(ShowSearchDialogAsync);
 
-            var _ = this.WhenValueChanged(mainWindowViewModel => mainWindowViewModel.CompetitionsViewModel.SelectedCompetitionProxy)
+            var _ = this.WhenValueChanged(mainWindowViewModel =>
+                    mainWindowViewModel.CompetitionsViewModel.SelectedCompetitionProxy)
                 .Select(competitionProxy => competitionProxy is not null)
                 .ToPropertyEx(this, mainWindowViewModel => mainWindowViewModel.CompetitionHasValue);
         }
 
-        private async Task ShowSearchDialogAsync() => await CompetitionsDialog.Handle(CompetitionsViewModel);
-
-        //[Reactive] public CompetitionProxy? Competition { get; set; }
+        public CompetitionsViewModel CompetitionsViewModel { get; }
 
         [ObservableAsProperty] public bool CompetitionHasValue { get; } = false;
-        
+
         public Interaction<CompetitionsViewModel, CompetitionProxy> CompetitionsDialog { get; } = new();
 
         public ReactiveCommand<Unit, Unit> SearchCompetitions { get; }
+
+        private async Task ShowSearchDialogAsync()
+        {
+            await CompetitionsDialog.Handle(CompetitionsViewModel);
+        }
     }
 }
