0e49f73fd05fe794052605921acfe7e9bf48684e
alex6dj <alexeiagueroalba@gmail.com>
2022-05-31 14:46:00 -0400

    ToUnit extension

---
+++ src/Common/Extensions/ReactiveEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Common.Extensions
+{
+    public static class ReactiveEx
+    {
+        public static IObservable<Unit> ToUnit<T>(this IObservable<T> observable)
+        {
+            return observable.Select(_ => Unit.Default);
+        }
+    }
+}
