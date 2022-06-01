9d90bb94ec7f8e5f9145b84f804c4ae257b1aa36
alex6dj <alexeiagueroalba@gmail.com>
2022-06-01 16:27:58 -0400

    Implement generic SerialDisposable

---
+++ src/Common/SerialDisposable.cs
@@ -0,0 +1,37 @@
+﻿using ReactiveUI;
+
+namespace System.Reactive.Disposables
+{
+    using System;
+
+    // generic variant of Rx's SerialDisposable class
+    // https://kent-boogaart.com/blog/serialdisposablet
+    public sealed class SerialDisposable<T> : ReactiveObject, ICancelable, IDisposable
+        where T : IDisposable
+    {
+        private readonly SerialDisposable _disposable;
+
+        public SerialDisposable()
+        {
+            _disposable = new SerialDisposable();
+        }
+
+        public bool IsDisposed => _disposable.IsDisposed;
+
+        public T? Disposable
+        {
+            get
+            {
+                return (T?)_disposable.Disposable;
+            }
+            set
+            {
+                _disposable.Disposable = value;
+                this.RaisePropertyChanged(nameof(Disposable));
+            }
+        }
+
+        public void Dispose()
+            => _disposable.Dispose();
+    }
+}
